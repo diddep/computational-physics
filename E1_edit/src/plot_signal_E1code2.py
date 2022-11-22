19894d9815f1264d954b843fb7ead3a01eb61c88
Didrik Palmqvist <didrikp@student.chalmers.se>
2022-11-22 11:01:54 +0100

    plot

--- E1_edit/src/plot_signal_E1code2.py
+++ E1_edit/src/plot_signal_E1code2.py
@@ -5,6 +5,9 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_theme()
+
 
 # skip_header skips the first
 # row in data.csv
@@ -19,20 +22,31 @@ ax.set_ylabel('signal (arb.unit)')
 ax.set_title('dt=0.1/6')
 ax.grid()
 
-fig.savefig('signalsuperposedt16.pdf')
+#fig.savefig('signalsuperposedt16.pdf')
 
 
-array_pow = np.genfromtxt('../powerspectrum_shift.csv', delimiter=',', skip_header=1)
-print(array_pow)
+array_pow = np.genfromtxt('../powerspectrum_shift_p1.csv', delimiter=',', skip_header=1)
+array_pow2 = np.genfromtxt('../powerspectrum_shift_p2.csv', delimiter=',', skip_header=1)
+array_pow3 = np.genfromtxt('../powerspectrum_shift_p3.csv', delimiter=',', skip_header=1)
+
+
 
 fig_pow, ax_pow = plt.subplots()
 
 
-ax_pow.plot(array_pow[:,1], array_pow[:,0])
+#ax_pow.plot(array_pow[:,1], array_pow[:,0])
+#ax_pow.plot(array_pow2[:,1], array_pow2[:,0])
+ax_pow.plot(array_pow3[:,1], array_pow3[:,0]+ array_pow[:,0]+array_pow2[:,0])
+
+print(array_pow3[:,0])
+
 ax_pow.set_xlabel('frequency (arb.unit)')
 ax_pow.set_ylabel('intensity (arb.unit)')
-ax_pow.set_title('dt=0.1/6')
-fig_pow.savefig('pow_spectrum_shift_superposdt16.pdf')
+ax_pow.set_title('Power spectrum for co2')
+ax_pow.set_xticks([-75,-39,39,75])
+ax_pow.set_xlim(-150,150)
+
+fig_pow.savefig('pow_spectrum_co2.pdf')
 
 
 plt.show()
---
+++ H1/src/plot_task1.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+array = np.genfromtxt('../try_lattice_constants.csv', delimiter=',').reshape(-1,5)
+
+
+fig_vol_en , ax = plt.subplots(1,1)
+
+model = np.poly1d(np.polyfit(array[:,2],array[:,4], 2))
+print(model)
+
+lin = np.linspace(np.min(array[:,2]), np.max(array[:,2]), 50)
+ax.scatter(array[:,2],array[:,4], label='datapoints')
+ax.plot(lin, model(lin), linestyle=':', label=f'Curvefit, \n y ={model[2]:.2} x^2+ {model[1]:.2} x +{model[0]:.2}')
+ax.set_ylabel('Volume Å^3', fontsize = 15)
+ax.set_xlabel('Energy (eV/unit cell)', fontsize = 15)
+plt.legend(fontsize=10)
+plt.tight_layout()
+plt.savefig('task1_plot')
+
+
+plt.show()
+
