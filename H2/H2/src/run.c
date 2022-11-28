6deae08117e66d8011b30baa6bef470c8b9838da
diddep <99744670+diddep@users.noreply.github.com>
2022-11-28 19:27:04 +0100

    Add files via upload

---
+++ H2/H2/src/run.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+int
+run(
+    int argc,
+    char *argv[]
+   )
+{
+    // Write your code here
+    // This makes it possible to test
+    // 100% of you code
+    return 0;
+}
