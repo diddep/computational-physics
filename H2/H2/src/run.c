2c227a3dc706fd1e9ba6f1a09ce6415ad4e2c605
Didrik Palmqvist <didrikp@student.chalmers.se>
2022-11-28 22:44:13 +0100

    rough version av mcmc för task 1 implemterad

--- H2/H2/src/run.c
+++ H2/H2/src/run.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+#include <time.h>
 
+#include "tools.h"
+#include "distribution.h"
+#include "Energy_calc.h"
 
 int
 run(
@@ -9,8 +15,43 @@ run(
     char *argv[]
    )
 {
-    // Write your code here
-    // This makes it possible to test
-    // 100% of you code
+    int N = 1000;
+    double alpha = 0.1;
+    double d = 1.0;
+    int acceptance =1;
+    double R1[N][3];
+    double R2[N][3];
+    double R1_test[3];
+    double R2_test[3];
+
+    for (int k; i<3, ++i){
+
+        R1[0][k] = d * ((double) rand(1000)- 500.0)/1000.0;
+        R2[0][k] = d * ((double) rand(1000)- 500.0)/1000.0;
+    }
+
+    for(int i=1, i<N; ++i){
+
+        for (int k; i<3; ++i){
+            R1_test = R1[i][k] + d * ((double) rand(1000)- 500.0)/1000.0;
+            R2_test = R2[i][k] + d * ((double) rand(1000)- 500.0)/1000.0;
+        }
+
+        double prob_test = distribution(R1_test, R2_test, alpha);
+        double prob_old = distribution(R1[0], R2[0], alpha);
+
+        if(prob_test>prob_old|| prob_test/prob_old >((double) rand(1000)- 500.0)/1000.0){
+            acceptance +=1;
+            for (int k; i<3, ++i){
+                R1[i+1][k] = R1_test[k];
+                R2[i+1][k] = R2_test[k];
+            }
+        }
+        else{
+            R1[i+1][k] = R1[i][k];
+            R2[i+1][k] = R2[i][k];
+
+        }
+    }
     return 0;
 }
