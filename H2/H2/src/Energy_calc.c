d945cf159235e99018b8766b665e9e696e595324
CarlStrandby <carstran@student.chalmers.se>
2022-11-30 16:41:40 +0100

    fixed

--- H2/H2/src/Energy_calc.c
+++ H2/H2/src/Energy_calc.c
@@ -16,29 +16,27 @@
 void Energy(double *E_L, double alpha, int N, double **R1, double **R2){
 
     double r12;
-    double *r1_nrm = malloc(sizeof(double)*3);
-    double *r2_nrm = malloc(sizeof(double)*3);
-    double *diff_vec = malloc(sizeof(double)*3);
-    double *diff_nrm = malloc(sizeof(double)*3);
-    double prod=0.;
-    double div = 0.;
+    double *r1_nrm = malloc(sizeof(double) * 3), *r2_nrm = malloc(sizeof(double) * 3);
+    double *diff_vec = malloc(sizeof(double) * 3), *diff_nrm = malloc(sizeof(double) * 3);
+    double prod = 0., div = 0.;
 
-    for (int i=0;i<N; ++i){
-
+    for (int i = 0; i < N; ++i){
         for (int dim=0; dim<3; ++dim){
             r1_nrm[dim] = R1[i][dim];
             r2_nrm[dim] = R2[i][dim];
         }
 
         normalize_vector(r1_nrm, 3);
-        normalize_vector(r2_nrm,3);
+        normalize_vector(r2_nrm, 3);
         r12 = distance_between_vectors(R1[i], R2[i], 3);
-        elementwise_subtraction( diff_vec, R1[i], R2[i], 3);
-        elementwise_subtraction( diff_nrm, r1_nrm, r2_nrm, 3);
-        prod = dot_product(diff_vec, diff_nrm,3);
-        div = (1. +alpha*r12);
 
-        E_L[i]=-4.0 + prod/(r12 * pow(div,2.0)) -1.0/(r12* pow(div,3.0)) - 1/(4.0* pow(div,4.0))+ 1/r12;
+        elementwise_subtraction(diff_vec, R1[i], R2[i], 3);
+        elementwise_subtraction(diff_nrm, r1_nrm, r2_nrm, 3);
+        prod = dot_product(diff_vec, diff_nrm,3);
+        div = (1. + alpha * r12);
+
+        E_L[i] = - 4.0 + prod/(r12 * pow(div, 2.0)) -1.0/(r12* pow(div, 3.0))\
+                 - 1./(4.0* pow(div,4.0))+ 1. / r12;
 
     }
     //r12 = NULL;
