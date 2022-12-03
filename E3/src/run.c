aba3eb26efafe9fd2eff3f3ef4a0176d0b0536e3
CarlStrandby <carstran@student.chalmers.se>
2022-12-03 18:42:28 +0100

    Added todo:s  for TODO Highlight extension

--- E3/src/run.c
+++ E3/src/run.c
@@ -23,6 +23,7 @@ void get_points(double *array_of_points, int number_of_points, double lower_boun
     gsl_rng_free(r);
 }
 
+// FIXME: Incorrect transform method (see page 9 in MC lecture notes)
 void pdf_transformed_points_task2(double *array_of_points, int number_of_points)
 {
     for(int ix = 0; ix < number_of_points; ix++)
@@ -36,6 +37,7 @@ double function_value_task1(double point_to_evaluate)
     return point_to_evaluate * (1 - point_to_evaluate);
 }
 
+// TODO: Update with new 3d function
 double function_value_task3(double point_to_evaluate)
 {
     return point_to_evaluate * (1 - point_to_evaluate);
@@ -46,11 +48,11 @@ double weight_function_task2(double point_to_evaluate)
     return sin(M_PI * point_to_evaluate);
 }
 
+// TODO: Update with new 3d function
 double weight_function_task3(double point_to_evaluate)
 {
     return sin(M_PI * point_to_evaluate);
 }
-
 
 void evaluate_integral(double *array_of_points, int number_of_points, double *integral_results, bool is_integral_1, bool is_weighted)
 {
@@ -89,10 +91,9 @@ void evaluate_integral(double *array_of_points, int number_of_points, double *in
     integral_results[1] = integral_std; 
 }
 
-void task1()
-{   
-    bool is_integral_1 = true, is_weighted = true;
-    int number_of_calculations = 4, number_of_results = 2;
+//TODO: Figure out logical struction so that MCMC_integration can be used for all 4 tasks.
+void MCMC_integration(bool is_integral_1, bool is_weighted, int number_of_calculations, int number_of_results)
+{
 
     double **calculation_results = create_2D_array(number_of_calculations, number_of_results);
     char filename_calculation_results[] = {"../task1/calculation_results_task1.csv"};
@@ -126,10 +127,26 @@ void task1()
     destroy_2D_array(calculation_results, number_of_calculations);
 }
 
+void task1()
+{
+    bool is_integral_1 = true, is_weighted = true;
+    int number_of_calculations = 4, number_of_results = 2;
+    MCMC_integration(is_integral_1, is_weighted, number_of_calculations,number_of_results  );
+
+}
+
+// TODO: Decide on whether to have separate function or reuse task1 with different bools
+void task3()
+{
+    bool is_integral_1 = false, is_weighted = true;
+    int number_of_calculations = 1, number_of_results = 2;
+    MCMC_integration(is_integral_1, is_weighted, number_of_calculations,number_of_results);
+}
+
 int
 run(int argc, char *argv[])
 {
-
-   task1();
-
+    task1();
+    //TODO: Uncomment when ready to run
+    //task3();
 }
