a87b913ff96fadeda3464e7dc5fb7af7bbd6f0f9
CarlStrandby <carstran@student.chalmers.se>
2022-12-03 15:30:13 +0100

    .json files updated to enable running with f5

---
+++ E3/src/run.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+#include <time.h>
+
+#include "tools.h"
+
+int
+run(int argc, char *argv[])
+{
+    printf("new run, latest change remove fsanitize\n");
+}
