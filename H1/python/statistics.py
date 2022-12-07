1c378e3453f04f4c08b85738ca294e4f970d1467
Didrik Palmqvist <didrikp@student.chalmers.se>
2022-12-07 12:06:06 +0100

    stuff

--- H1/python/statistics.py
+++ H1/python/statistics.py
@@ -8,25 +8,33 @@ str = "eq"
 array = np.genfromtxt(f'../csv/vel_verlet_{str}.csv', delimiter=',')
 parameters = np.genfromtxt(f'../csv/parameters_{str}.csv', delimiter=',')
 
+print(array.shape)
 t = array[-1:,0]
-e_pot = array[-1:,2]
-e_kin = array[-1:,3]
-e_tot = array[-1:,4]
+#e_pot = array[-1:,2]
+#e_kin = array[-1:,3]
+#e_tot = array[-1:,4]
+
+e_pot = array[:,2]
+e_kin = array[:,3]
+e_tot = array[:,4]
+
+
 temp_eq = parameters[-1,5]
 
-
+print(e_kin.shape, np.var(e_kin))
 k_B= 8.61733 * 1e-5; N = 256
 
 
 
 def C_v(epsilon_vec, T):
 
-    variance = np.var(e_kin)
+    variance = np.var(epsilon_vec)
+    print(variance,'var')
     term = 1-2*variance/(3*N*(k_B*T)**2)
 
     C_v = 3*N*k_B/term
 
-    return
+    return C_v
 
 C_v_from_kinetic = C_v(e_kin,temp_eq)
 C_v_from_potential = C_v(e_pot,temp_eq)
--- H1/src/tools.c
+++ H1/src/tools.c
@@ -331,3 +331,27 @@ double variance(double *quantity_vec, int number_of_elements)
     return variance;
 }
 
+// Function to calculate the inverse of a matrix
+void inverse(double matrix[N][N])
+{
+    // Find the determinant of the matrix
+    double det = 0;
+    for (int i = 0; i < N; i++)
+        det += matrix[0][i] * (matrix[1][(i+1)%N] * matrix[2][(i+2)%N] - matrix[1][(i+2)%N] * matrix[2][(i+1)%N]);
+
+    // Create a new matrix to hold the inverse
+    double inverse[N][N];
+
+    // Find the inverse of the matrix
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            inverse[i][j] = ((matrix[(j+1)%N][(i+1)%N] * matrix[(j+2)%N][(i+2)%N]) - (matrix[(j+1)%N][(i+2)%N] * matrix[(j+2)%N][(i+1)%N])) / det;
+
+    // Print the inverse of the matrix
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+            printf("%lf ", inverse[i][j]);
+        printf("\n");
+    }
+}
