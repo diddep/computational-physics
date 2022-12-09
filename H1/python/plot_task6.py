67bee3f14d27c04028afd4771bd742a114853509
Didrik Palmqvist <didrikp@student.chalmers.se>
2022-12-09 15:02:07 +0100

    grejer för task 6

--- H1/python/plot_task6.py
+++ H1/python/plot_task6.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import scipy as sp
 sns.set()
 
 Cell_length = 4*4.03; number_particles = 256
@@ -8,7 +9,6 @@ Cell_length = 4*4.03; number_particles = 256
 array = np.genfromtxt(f'../csv/radial_distribution.csv', delimiter=',', skip_header=0)
 
 number_of_bins = len(array)
-print(number_of_bins)
 
 def N_ideal(bin_arr, Cell_length, Number_of_particles):
     Number_of_bins = len(bin_arr)
@@ -29,25 +29,38 @@ delta_r = Cell_length/number_particles
 fig, ax_radial = plt.subplots(1,1)
 array = (array/ideal)
 
-max =np.max(array)
-conv = 1#3/max
+max =np.argmax(array)
+conv = 1/1.8#3/max
 
-ax_radial.plot(bin_array*delta_r, array*conv)
-ax_radial.set_xlim(0,4)
+ax_radial.plot(2*bin_array*delta_r, array*conv)
+ax_radial.set_xlim(0,8)
 
-minima = array[array > 0].min()
+
+
+max = np.argmax(array)
+print(max)
+minima = np.argmin(array[max:])
+print(minima*delta_r)
+#print(array[15:80])
+
+ax_radial.axvline(minima*delta_r)
+
+print(array[:minima])
+
+integrand =[]
+#34
+
+for i,r in enumerate(2*bin_array*delta_r):
+    print(r,i, array[i])
+    integrand.append(r**2* 4*np.pi* array[i]*conv)
+
+#print(integrand)
+
+#coordination_number = sp.integrate.trapezoid(2*bin_array[max]*delta_r, integrand)
+
+#print(coordination_number)
 
 # Annotate the plot with an arrow pointing to the minimum
-ax_radial.annotate(
-    'First non-zero minimum',  # text to display
-    xy=(minima, 0),           # coordinates of the arrow's tail
-    xytext=(minima, 1),       # coordinates of the arrow's head
-    arrowprops=dict(           # arrow style
-        facecolor='black',
-        shrink=0.05,
-        width=2,
-        headwidth=8
-    )
-)
+
 
 fig.savefig(f'plots/radial.png')
