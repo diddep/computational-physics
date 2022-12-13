c67119d416716fc5dabd6dd3fcb86c6e0e90b072
Didrik Palmqvist <didrikp@student.chalmers.se>
2022-12-13 13:49:00 +0100

    test gitignore

---
+++ H2/obj/restructured_stat_ineff.d
@@ -0,0 +1,2 @@
+obj/restructured_stat_ineff.o: src/restructured_stat_ineff.c \
+ include/tools.h
---
+++ H2/obj/run.d
@@ -0,0 +1,3 @@
+obj/run.o: src/run.c include/tools.h include/distribution.h \
+ include/MCMC_chain_operations.h include/statistical_ineff.h \
+ include/restructured_stat_ineff.h
