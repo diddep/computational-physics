6f07e472569c737fb02f83944af070570533cf4f
Didrik Palmqvist <didrikp@student.chalmers.se>
2022-12-13 19:01:13 +0100

    added the new gradient

--- H2/src/MCMC_chain_operations.c
+++ H2/src/MCMC_chain_operations.c
@@ -47,42 +47,71 @@ void Energy(double *E_local, double alpha, int N_steps, double **R1, double **R2
     free(r1_nrm), free(r2_nrm), free(diff_nrm), free(diff_vec);
 }
 
-double partialEnergyDerivative(double *E_local_derivative, double alpha, int N_steps, double **R1, double **R2){
+// double partialEnergyDerivative(double *E_local_derivative, double alpha, int N_steps, double **R1, double **R2){
 
-    double r12;
-    double *r1_nrm = malloc(sizeof(double) * NDIM), *r2_nrm = malloc(sizeof(double) * NDIM);
-    double *diff_vec = malloc(sizeof(double) * NDIM), *diff_nrm = malloc(sizeof(double) * NDIM);
-    //double *E_local_derivative;// = malloc(sizeof(double) * N_steps);
-    double prod = 0., div = 0., E_derivative_sum = 0;
+//     double r12;
+//     double *r1_nrm = malloc(sizeof(double) * NDIM), *r2_nrm = malloc(sizeof(double) * NDIM);
+//     double *diff_vec = malloc(sizeof(double) * NDIM), *diff_nrm = malloc(sizeof(double) * NDIM);
+//     //double *E_local_derivative;// = malloc(sizeof(double) * N_steps);
+//     double prod = 0., div = 0., E_derivative_sum = 0;
 
-    for (int ix = 0; ix < N_steps; ++ix){
-        for (int dim=0; dim<NDIM; ++dim){
-            r1_nrm[dim] = R1[ix][dim];
-            r2_nrm[dim] = R2[ix][dim];
-        }
+//     for (int ix = 0; ix < N_steps; ++ix){
+//         for (int dim=0; dim<NDIM; ++dim){
+//             r1_nrm[dim] = R1[ix][dim];
+//             r2_nrm[dim] = R2[ix][dim];
+//         }
 
-        normalize_vector(r1_nrm, NDIM);
-        normalize_vector(r2_nrm, NDIM);
-        r12 = distance_between_vectors(R1[ix], R2[ix], NDIM);
+//         normalize_vector(r1_nrm, NDIM);
+//         normalize_vector(r2_nrm, NDIM);
+//         r12 = distance_between_vectors(R1[ix], R2[ix], NDIM);
 
-        elementwise_subtraction(diff_vec, R1[ix], R2[ix], NDIM);
-        elementwise_subtraction(diff_nrm, r1_nrm, r2_nrm, NDIM);
-        prod = dot_product(diff_vec, diff_nrm,NDIM);
-        div = (1. + alpha * r12);
+//         elementwise_subtraction(diff_vec, R1[ix], R2[ix], NDIM);
+//         elementwise_subtraction(diff_nrm, r1_nrm, r2_nrm, NDIM);
+//         prod = dot_product(diff_vec, diff_nrm,NDIM);
+//         div = (1. + alpha * r12);
 
-        E_local_derivative[ix] = - 0.0 \
-                                - 2 * r12 * prod/(r12 * pow(div, 3.0)) \
-                                + 3 * r12 * 1.0/(r12* pow(div, 4.0)) \
-                                + 4 * r12 * 1./(4.0* pow(div, 5.0)) 
-                                + 0.0;
+//         E_local_derivative[ix] = - 0.0 \
+//                                 - 2 * r12 * prod/(r12 * pow(div, 3.0)) \
+//                                 + 3 * r12 * 1.0/(r12* pow(div, 4.0)) \
+//                                 + 4 * r12 * 1./(4.0* pow(div, 5.0)) 
+//                                 + 0.0;
 
-        E_derivative_sum += E_local_derivative[ix];
+//         E_derivative_sum += E_local_derivative[ix];
+//     }
+
+//     free(r1_nrm), free(r2_nrm), free(diff_nrm), free(diff_vec);
+
+//     return E_derivative_sum/N_steps;
+// }
+
+//TODO kan göras snabbare om vi skickar in E_local chain istället
+double partialEnergyDerivative(double *E_local_derivative, double alpha, int N_steps, double **R1, double **R2)
+{
+    double *E_local_chain = malloc(sizeof(double)*N_steps);
+    double r12=0, ln_d_psi=0;
+    double average_E=0, average_derivative =0, average_mix=0;
+    double gradient=0;
+    
+    Energy(E_local_chain, alpha, N_steps, R1, R2);
+
+    for(int step=0; step<N_steps; ++step)
+    {
+        r12 = distance_between_vectors(R1[step], R2[step], NDIM);
+        ln_d_psi = - r12*r12/(2*pow((r12*alpha +1),2));
+        average_derivative += ln_d_psi;
+        average_mix += ln_d_psi*E_local_chain[step];
+        average_E += E_local_chain[step];
     }
+    average_derivative /= N_steps;
+    average_mix /= N_steps;
+    average_E /= N_steps; 
 
-    free(r1_nrm), free(r2_nrm), free(diff_nrm), free(diff_vec);
+    gradient = 2*(average_mix - average_E*average_derivative);
+    free(E_local_chain);
 
-    return E_derivative_sum/N_steps;
+    return gradient;
 }
+
 
 /*function calculating distribution of x from two position mcmc chains
  * args:
